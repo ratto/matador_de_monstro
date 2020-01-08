1668e3bd5fdbe4865a2361ab2b09cab64df41f3c
ratto <rattopedro@gmail.com>
2020-01-08 01:32:30 -0300

    Mecânica de batalha criada

--- js/app.js
+++ js/app.js
@@ -1,34 +1,35 @@
 new Vue({
-    el: "app",
-    data: {
-        jogador: {
-            vida: 100,
-            ataqueMin: 1,
-            ataqueMax: 12
-        },
-        monstro: {
-            vida: 100,
-            ataqueMin: 5,
-            ataqueMax: 15
+    el: "#app",
+    data() {
+        return {
+            jogador: {
+                vida: 100,
+                ataqueMin: 1,
+                ataqueMax: 12,
+            },
+            monstro: {
+                vida: 100,
+                ataqueMin: 5,
+                ataqueMax: 15,
+            }
         }
     },
     methods: {
         atacar: function() {
-            monstro.vida -= ataqueTotal(jogador.ataqueMin, jogador.ataqueMax);
-            jogador.vida -= ataqueTotal(monstro.ataqueMin, monstro.ataqueMax);
+            this.monstro.vida -= this.ataqueTotal(this.jogador.ataqueMin, this.jogador.ataqueMax);
+            this.jogador.vida -= this.ataqueTotal(this.monstro.ataqueMin, this.monstro.ataqueMax);
         },
 
         ataqueEspecial: function() {
-            monstro.vida -= ataqueTotal(jogador.ataqueMin + 5, jogador.ataqueMax + 5);
-            jogador.vida -= ataqueTotal(monstro.ataqueMin, monstro.ataqueMax);
+            this.monstro.vida -= this.ataqueTotal(this.jogador.ataqueMin + 5, this.jogador.ataqueMax + 5);
+            this.jogador.vida -= this.ataqueTotal(this.monstro.ataqueMin, this.monstro.ataqueMax);
         },
 
         curar: function() {
-            jogador.vida += ataqueTotal(jogador.ataqueMin + 2, jogador.ataqueMax + 1);
-            jogador.vida -= ataqueTotal(monstro.ataqueMin, monstro.ataqueMax);
+            this.jogador.vida += this.ataqueTotal(this.jogador.ataqueMin + 5, this.jogador.ataqueMax + 5);
+            this.jogador.vida -= this.ataqueTotal(this.monstro.ataqueMin, this.monstro.ataqueMax);
         },
-    },
-    computed: {
+
         ataqueTotal(min, max) {
             return Math.floor(Math.random() * max) + min;
         }  
